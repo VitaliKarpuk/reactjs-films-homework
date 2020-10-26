711d7adaaaf085e76c7b4c0dd14f92eddd3af2e0
Vitali Karpuk <karpuk-job@yandex.ru>
2020-10-26 16:04:14 +0300

    feat: express server

---
+++ config/webpack/webpack.dev.config.js
@@ -0,0 +1,92 @@
+/* eslint-disable no-undef */
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    main: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000', './src/index.js']
+  },
+  output: {
+    path: path.join(__dirname, '../../build'),
+    publicPath: '/',
+    filename: '[name].js'
+  },
+  mode: 'development',
+  target: 'web',
+  devtool: '#source-map',
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+        options: {
+          emitWarning: true,
+          failOnError: false,
+          failOnWarning: false
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          }
+        ]
+      },
+      { 
+        test: /\.(s[ca]ss)/,
+        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+      },
+      // {
+      //   test: /\.(s[ca]ss)/,
+      //   use: [
+      //     MiniCssExtractPlugin.loader,
+      //     'css-loader',
+      //     'sass-loader',
+      //   ],
+      // },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [{
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "./assets/images",
+          }
+        }]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)$/,
+        use: [{
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "./assets/fonts",
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+      excludeChunks: ['server']
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    // new MiniCssExtractPlugin({
+    //   filename: "./css/[name].css",
+    //   chunkFilename: "[id].css"
+    // }),
+  ]
+}
