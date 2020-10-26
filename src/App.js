d41ff7782fa32e693917a159771464a04af30d49
Vitali Karpuk <karpuk-job@yandex.ru>
2020-10-26 16:06:10 +0300

    feat: test script

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MovieDetailsPage  from './pages/MovieDetailsPage/MovieDetailsPage';
+
+const App = () => {
+  console.log('ffff');
+  return (
+    <div className="content">
+      <h1>fgfdgfh</h1>
+      <MovieDetailsPage />
+    </div>
+  )
+}
+
+export default App;
