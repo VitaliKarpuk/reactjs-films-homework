7ca09f99fc248071b69840345d19dd16be82f904
Vitali Karpuk <karpuk-job@yandex.ru>
2020-11-07 13:40:35 +0300

    feat: search

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,22 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 const SearchInput = ({ onSearchFilms }) => {
-  const [value, setValue] = useState('');
-  const onChangeValue = (e) => {
-    setValue(e.target.value)
-  }
+  const [value, setValue] = useState("");
+  const onChangeValue = e => {
+    setValue(e.target.value);
+  };
 
   const onHandleEnter = ({ key }) => {
-    if (key === 'Enter') {
-      onSearchFilms(value)
+    if (key === "Enter") {
+      onSearchFilms(value);
     }
-  }
+  };
   return (
     <div className="header__search">
-      <input type="text" placeholder="the jungle book" value={value} onChange={onChangeValue} onKeyPress={onHandleEnter} />
-      <span className='header__search-icon'></span>
+      <input
+        type="text"
+        placeholder="the jungle book"
+        value={value}
+        onChange={onChangeValue}
+        onKeyPress={onHandleEnter}
+      />
+      <span className="header__search-icon"></span>
     </div>
-  )
-}
+  );
+};
+
+SearchInput.propTypes = {
+  onSearchFilms: PropTypes.func
+};
 
 export default SearchInput;
