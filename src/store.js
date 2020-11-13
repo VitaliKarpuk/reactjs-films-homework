10b4f5d7c39d22299186f75c35b4599aad682956
Vitali Karpuk <karpuk-job@yandex.ru>
2020-11-13 19:59:24 +0300

    refactor: store

--- src/store.js
+++ src/store.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import thunkMiddleware from 'redux-thunk'
+import thunkMiddleware from 'redux-thunk';
 import reducer from './modules/reducer/reducer';
 
+const middleware = [
+  thunkMiddleware,
+];
 
 const store = createStore(
   reducer,
-  composeWithDevTools(applyMiddleware(thunkMiddleware )),
+  composeWithDevTools(applyMiddleware(...middleware)),
 );
 
 export default store;
