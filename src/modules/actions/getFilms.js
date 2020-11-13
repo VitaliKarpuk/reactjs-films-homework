205ebc0f3d4a68d19345e8bde5d3f580adeb6c50
Vitali Karpuk <karpuk-job@yandex.ru>
2020-11-13 13:13:21 +0300

    feat: add async action

--- src/modules/actions/getFilms.js
+++ src/modules/actions/getFilms.js
@@ -1,16 +1,18 @@
 import { GET_FILMS, API_KEY } from '../constants/constants';
 import getGenre from './getGenre';
 
-const getFilms = () => async (dispatch) => {
-  await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
-    .then(resp => resp.json())
-    .then(data => dispatch({
-      type: GET_FILMS,
-      payload: data.results
-    }))
-    .then(() => {
-      dispatch(getGenre())
-    })
+const getFilms = () => {
+  return async (dispatch) => {
+    await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+      .then(resp => resp.json())
+      .then(data => dispatch({
+        type: GET_FILMS,
+        payload: data.results
+      }))
+      .then(() => {
+        dispatch(getGenre())
+      })
+  }
 }
 
 export default getFilms;
